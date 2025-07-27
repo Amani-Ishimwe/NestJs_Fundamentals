25d9ead31cf52207d0c9be077f03fcf9c307a20e
Amani Ishimwe <amaniishimwe36@gmail.com>
2025-07-27 21:24:19 +0300

    adding authentication services

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { User } from 'generated/prisma';
+import * as bcrypt from 'bcrypt'
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor (private readonly usersService: UsersService) {}
+
+    async validateUser(
+        username:string,
+        password:string
+    ):Promise<Omit<User,'password'>|null>{
+        const user = await this.usersService.findOne({ username})
+        if(user?.password == null){
+            return null;
+        }
+
+        const isPasswordMatch = await bcrypt.compare(password,user.password)
+        if(isPasswordMatch){
+            return {
+                id: user.id,
+                username: user.username
+            }
+        }
+        return null;
+    }
+}
